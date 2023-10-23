fadb43188a575eb7d890c22b5b5450c9a8ac4380
BlopApple <junjie1363@gmail.com>
2023-10-23 12:18:50 +0800

    Add templates for NN models

---
+++ model.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+# import torch.nn.functional as F
+
+class ToneEval_Base(nn.Module):
+    '''
+    This the base model for Tone Evaluation.
+    '''
+
+    def __init__(self):
+        '''
+        Define Layers
+        '''
+        super().__init__()
+
+    def forward(self, x):
+
+        return None
+
+class ToneEval_Transformer(nn.Module):
+    '''
+    This the attention-based transformer model for Tone Evaluation.
+    '''
+
+    def __init__(self):
+        '''
+        Define Layers
+        '''
+        super().__init__()
+
+    def forward(self, x):
+
+        return None
