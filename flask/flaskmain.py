59d299bd999e0bc7059ee5e3dd45228cc913014b
trufflepirate <rootfourtytwo@gmail.com>
2023-11-23 13:58:09 +0800

    flask

--- flask/flaskmain.py
+++ flask/flaskmain.py
@@ -2,8 +2,47 @@ import base64
 from flask import Flask, render_template, request
 from flask import request
 
+import librosa,io
+import sys
+import os
+import pinyin
+import json
+sys.path.append(os.path.abspath('../'))
+
+from model import ToneEval_Base
+from dataset import load_segmentation_model,single_segmentation_no_load
+
+
+import torch
+
+def loadclassificationsystem():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_classify = ToneEval_Base(input_shape=(1, 128, 75))
+    processor_segement, model_segment = load_segmentation_model()
+
+    model_classify.load_state_dict(torch.load('../results/1024_lr-0.001/best_model.pth'))
+
+    model_classify.to(device)
+    model_segment.to(device)
+    return model_classify,model_segment,processor_segement,device
+
+MODEL_CLASSIFY,MODEL_SEGMENT,PROCESSOR_SEGMENT,DEVICE = loadclassificationsystem()
+
+def run_segmentation(audio,MODEL_SEGMENT,PROCESSOR_SEGMENT, original_len = 2):
+     audio = torch.from_numpy(audio).float()
+     predictions, transcription, times = single_segmentation_no_load(audio,PROCESSOR_SEGMENT, MODEL_SEGMENT,originallen= original_len, DEVICE=DEVICE)
+     print(predictions,transcription,times)
+     return predictions, transcription,times
+
+# # model.load_state_dict(torch.load('results/1024_lr-0.001/best_model.pth'))
+# aud,sr = librosa.load("audio.wav", sr = 16000, mono=True)
+
+# print(aud.shape)
+# run_segmentation(aud,MODEL_SEGMENT,PROCESSOR_SEGMENT)
+# exit()
+
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
 @app.route('/')
@@ -12,13 +51,26 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    print(request.files)
-    audio = request.files['audio']
-    print(audio)
-    audio.save('audio.wav')
-    # Process the audio file here
-    # print(audio)
-    return 'Audio file received'
+
+    # audio = request.get_json()['audio']
+    # base64_audio = base64.b64decode(audio)
+    # buffer = io.BytesIO(base64_audio)
+    # write buffer to file
+    # with open('audio.wav', 'wb') as f:
+    #     f.write(base64_audio)
+    #     f.close()
+    # aud,sr = librosa.load(buffer, sr = 16000)
+    aud,sr = librosa.load("audio.wav", sr = 16000, mono=True)
+    
+    predictions, transcription,times = run_segmentation(aud,MODEL_SEGMENT,PROCESSOR_SEGMENT)
+    pyn = pinyin.get(transcription[0], format="numerical", delimiter=" ")
+    d = {
+        "transcription": transcription[0],
+        "times": times[0],
+        "pinyin": pyn
+    }
+
+    return json.dumps(d)
 
 if __name__ == '__main__':
-    app.run(host = "localhost", port=12345, threaded=True)
+    app.run( port=12345)
--- flask/static/stuff.js
+++ flask/static/stuff.js
@@ -1,39 +1,65 @@
 class MicrophoneRecorder {
     constructor() {
-        this.mediaStream = null;
-        this.mediaRecorder = null;
-        this.chunks = [];
-        this.isRecording = false;
+
+        this.webAudioRecorder; // our WebAudioRecorder.js recorder yet to be instantiated
+        this.currentlyRecording = false; // a boolean to keep track of whether recording is taking place
+        this.getUserMediaStream; // our stream from getUserMedia
     }
 
     async startRecording() {
-        if (this.isRecording) {
-            console.log('Microphone recording is already started.');
-            return;
-        }
+        if (this.currentlyRecording === false) {
+            // the built-in method for capturing audio/video from the user's device
+            // pass in the media capture options object and ask for permission to access the microphone
+            let options = { 'audio': true, 'video': false };
+            navigator.mediaDevices.getUserMedia(options)
+            .then(stream => {
+        
+                this.currentlyRecording = true;
+            
+              this.getUserMediaStream = stream;
 
-        try {
-            this.mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            this.mediaRecorder = new MediaRecorder(this.mediaStream);
+              let AudioContext = window.AudioContext ||  window.webkitAudioContext;
+              let audioContext = new AudioContext();
+              let source = audioContext.createMediaStreamSource(stream);
+              this.webAudioRecorder = new WebAudioRecorder(source, {
+                // workerDir: the directory where the WebAudioRecorder.js file lives
+                workerDir: 'static/',
+                // encoding: type of encoding for our recording ('mp3', 'ogg', or 'wav')
+                encoding: 'wav',
+                options: {
+                  // encodeAfterRecord: our recording won't be usable unless we set this to true
+                  encodeAfterRecord: true,
+                  // mp3: bitRate: '160 is default, 320 is max quality'
+                  mp3: { bitRate: '320' },
+                  sampleRate: 16000 // Set the sample rate to 16000    
+                }
+              });
 
-            this.mediaRecorder.addEventListener('dataavailable', (event) => {
-                this.chunks.push(event.data);
-                // console.log(this.chunks);
+              this.webAudioRecorder.onComplete = (webAudioRecorder, blob) => {
+                this.sendBlob(blob);
+              }
+
+              this.webAudioRecorder.onError = (webAudioRecorder, err) => {
+                  console.error(err);
+              }
+              // method that initializes the recording
+              this.webAudioRecorder.startRecording();
+            }).catch(err => {
+                console.error(err);
             });
+          } else {
+            // if we're already recording, stop the recording and stop the stream from getUserMedia
+            console.log('already recording');
+          }
 
-            this.mediaRecorder.start();
-            this.isRecording = true;
-            this.chunks = []; // Clean the chunks array when recording starts
-        } catch (error) {
-            console.error('Error starting microphone recording:', error);
-        }
     }
 
     stopRecording() {
-        if (this.mediaRecorder && this.mediaRecorder.state === 'recording') {
-            this.mediaRecorder.stop();
-            this.mediaStream.getTracks().forEach((track) => track.stop());
-            this.isRecording = false;
+        if (this.currentlyRecording === true) {
+            let audioTrack = this.getUserMediaStream.getAudioTracks()[0];
+            audioTrack.stop();
+            this.webAudioRecorder.finishRecording();
+            this.currentlyRecording = false;
         }
     }
 
@@ -42,44 +68,56 @@ class MicrophoneRecorder {
             await new Promise(resolve => setTimeout(resolve, 100)); // Wait for 100 milliseconds
         }
         // console.log(this.chunks);
-        // const blob = new Blob(this.chunks, { type: 'audio/webm' });
-        this.sendBlob(this.chunks[0]); 
+        const blob = new Blob(this.chunks, { type: 'audio/mpeg' }); // Use audio/mpeg MIME type for MP3
+        this.sendBlob(blob);
         return blob;
     }
 
     sendBlob(blob) {
-        const currentEndpoint = '/upload'; 
+        const currentEndpoint = window.location.origin + '/upload';
         console.log(blob);
-        // Create a FormData object
-        const formData = new FormData();
-        formData.append('audio', blob, 'recording.webm');
+        
+        // Create a FileReader object
+        const reader = new FileReader();
 
-        // Send the FormData object to the current endpoint using a POST request
-        fetch(currentEndpoint, {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (response.ok) {
-                console.log('Blob sent successfully to', currentEndpoint);
-            } else {
-                console.error('Failed to send blob to', currentEndpoint);
-            }
-        })
-        .catch(error => {
-            console.error('Error sending blob:', error);
-        });
+        // Read the blob as Data URL
+        reader.readAsDataURL(blob);
+
+        // When the reading is complete
+        reader.onloadend = () => {
+            // Get the base64 string
+            const base64String = reader.result.split(',')[1];
+
+            // Send the base64 string to the current endpoint using a POST request
+            fetch(currentEndpoint, {
+                method: 'POST',
+                body: JSON.stringify({ audio: base64String }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Base64 string sent successfully to', currentEndpoint);
+                } else {
+                    console.error('Failed to send base64 string to', currentEndpoint);
+                }
+                return response.json(); // Parse response as JSON
+            })
+            .then(data => {
+                console.log('Response:', data); // Log the response data
+            })
+            .catch(error => {
+                console.error('Error sending base64 string:', error);
+            });
+        };
     }
-
-    
 }
 
 const recorder = new MicrophoneRecorder();
-const RECORDER = recorder
+const RECORDER = recorder;
 recorder.startRecording();
-
 
 recorder.stopRecording();
 // const recording = recorder.getRecording();
 // console.log(recording);
-
